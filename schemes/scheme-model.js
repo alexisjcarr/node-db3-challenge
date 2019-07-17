7fa4ca952a63d1428a830da114e347f41eb7651f
alexisjcarr <acarr59@gmail.com>
2019-07-17 16:00:34 -0500

    ⚡️ Adds findSteps(id)
     #1

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -22,12 +22,32 @@ async function findById(id) {
   );
 }
 
-async function findSteps() {}
+async function add(scheme) {
+  const id = await db("schemes").insert(scheme, "id");
+  return findById(...id);
+}
 
-async function add() {}
+async function update(changes, id) {
+  await db("schemes")
+    .where({ id })
+    .update(changes);
+  return findById(id);
+}
+
+async function findSteps(id) {
+  // SELECT s.id, s.scheme_name, st.step_number, st.instructions FROM
+  // schemes AS s INNER JOIN steps AS st ON s.id = st.scheme_id
+  return await db("schemes")
+    .join("steps", "steps.scheme_id", "schemes.id")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .where("schemes.id", id);
+}
 
 async function addStep() {}
-
-async function update() {}
 
 async function remove() {}
